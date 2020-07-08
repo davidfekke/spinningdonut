30f5ae89762333ac51fb9db031ca9211b6f3314f
davidfekke <david@fekke.com>
2020-07-07 20:31:55 -0400

    Initial commit

---
+++ CMakeFiles/donut.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/donut.dir/donut.c.o"
+  "donut.pdb"
+  "donut"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/donut.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
